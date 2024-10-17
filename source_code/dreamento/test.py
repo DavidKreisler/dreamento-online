b51781be88b804ea783b8ec01d56a7c7a65fbd7c
David Kreisler <a01569177@unet.univie.ac.at>
2024-10-17 12:37:50 +0200

    Revert "relocation to other computer"
    This reverts commit c9985ff89d8165b88c4bcdfdd6ad1b9e7ecf5d97.

--- source_code/dreamento/test.py
+++ source_code/dreamento/test.py
@@ -3,9 +3,6 @@ import json
 import os
 
 import numpy as np
-import scipy.signal
-import matplotlib.pyplot as plt
-
 from source_code.dreamento.scripts.SleepScoring.SleePyCoInference import SleePyCoInference
 import torch
 
@@ -19,14 +16,15 @@ from models.main_model import MainModel
 #   11=oxy_ir_ac, 12=oxy_r_ac, 13=oxy_dark_ac,
 #   14=oxy_ir_dc, 15=oxy_r_dc, 16=oxy_dark_dc
 # ]
-from source_code.dreamento.scripts.Utils.ESleepStages import ESleepState
 
 
-def read_recording(path: str):
-    samples_db = np.load(path + 'samples_db.npy')
+
+def read_recording():
+    recording_base_path = 'recordings/recording-date-2024-08-05-time-20-18-55/'
+    samples_db = np.load(recording_base_path + 'samples_db.npy')
     eegr = []
     eegl = []
-    with open(path + 'complete.txt', 'r') as f:
+    with open(recording_base_path + 'complete.txt', 'r') as f:
         i = 0
         for line in f.readlines():
             elems = line.split(',')
@@ -37,50 +35,24 @@ def read_recording(path: str):
 
     return eegr, eegl, samples_db
 
-def get_time_from_seconds(seconds):
-    s = int((seconds % (60*60))) % 60
-    m = int((seconds - s) / 60) % 60
-    h = int((seconds - s - 60*m) / 3600)
-
-    return (f'{h}:{m}:{s}')
-
-
-
 if __name__ == '__main__':
-    # model
+    eegr, eegl, samples_db = read_recording()
+    epoch_length_in_samples = 30 * 256 # time_in_seconds * sample_rate
+
+    eegr_30_sec = eegr[:epoch_length_in_samples]
+    eegl_30_sec = eegl[:epoch_length_in_samples]
+
     config_path = 'scripts/SleepScoring/SleePyCo/SleePyCo/configs/SleePyCo-Transformer_SL-10_numScales-3_Sleep-EDF-2018_freezefinetune.json'
     with open(config_path, 'r') as config_file:
         config = json.load(config_file)
     config['name'] = os.path.basename(config_path).replace('.json', '')
 
-    model = SleePyCoInference(1, config)
+    inf = SleePyCoInference(1, config)
+    input = np.array(eegr_30_sec).reshape((1,1,len(eegr_30_sec)))
+    pred = inf.infere(input)
 
-    # recording
-    eegr, eegl, samples_db = read_recording('recordings/recording-date-2024-10-09-time-01-36-28/')
+    print(pred)
 
-    epoch_length_in_samples = 30 * 256# time_in_seconds * sample_rate * number of epochs
-
-    start_idx = 0
-    end_idx = epoch_length_in_samples
-    predictions = []
-    predictions_resampled = []
-
-    while end_idx < len(eegr):
-        eeg_30_sec = eegr[start_idx:end_idx]
-        signal_resampled = scipy.signal.resample(eeg_30_sec, int(100 / 256 * len(eeg_30_sec)))
-
-        input = np.array(eeg_30_sec).reshape(((1,1,len(eeg_30_sec))))
-        input_resampled = np.array(signal_resampled).reshape((1, 1, len(signal_resampled)))
-
-        pred = int(model.infere(input)[0])
-        pred_resampled = int(model.infere(input_resampled)[0])
-
-        predictions.append((get_time_from_seconds(start_idx/256), ESleepState(int(pred))))
-        predictions_resampled.append((get_time_from_seconds(start_idx / 256), ESleepState(int(pred_resampled))))
-
-        start_idx += int(epoch_length_in_samples/2)
-        end_idx += int(epoch_length_in_samples/2)
-
-    print(predictions)
-    print(predictions_resampled)
-
+    # len(samples_db) is total length of signal in seconds
+    # samples_db tells what line belongs to what second
+    # this may be off due to the socket and not taking into account the sampling rate there
